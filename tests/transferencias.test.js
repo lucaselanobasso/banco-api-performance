04f6f994e534c50e52b7763c4ff1d3f81c692a18
Lucas <lucaselanobasso@gmail.com>
2025-07-18 15:05:43 -0300

    Adicionando a função de helper para obtenção de token e criando o teste de post transferencia

---
+++ tests/transferencias.test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { obterToken } from '../helpers/autenticacao.js';
+const postTransferencias = JSON.parse(open('../fixtures/postTransferencias.json'))
+
+
+export const options = {
+  iterations: 1
+};
+
+export default function() {
+  const token = obterToken()
+
+  const url = 'http://localhost:3000/transferencias'
+
+  const payload = JSON.stringify(postTransferencias)
+
+  const params = {
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+        },
+    }
+  
+    const res = http.post(url, payload, params)
+
+    check(res, {
+      "status é 201": (res) => res.status === 201
+    })
+
+    sleep(1)
+}
